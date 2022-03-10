2e52f6a47c70d61f05566c832fbb98089679f025
Aleksandr Khromov <4-09@bk.ru>
2022-03-10 14:40:48 +0700

    added export fcm

--- lib/pushler_core.dart
+++ lib/pushler_core.dart
@@ -3,3 +3,4 @@ library pushler_core;
 export 'src/core/exception/exception.dart';
 export 'src/core/models/model.dart';
 export 'src/core/pushler_core.dart';
+export 'package:firebase_messaging/firebase_messaging.dart';
